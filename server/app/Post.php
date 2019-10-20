d001e2dbd96e64bcf676adef02f288db35ee25ef
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-10-20 14:38:22 +0900

    create model

--- server/app/Post.php
+++ server/app/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
--- server/app/User.php
+++ server/app/User.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    protected $fillable = [
+        'email',
+        'password',
+        'display_name',
+        'gender_id',
+    ];
+
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
