3f931402064c5e0c877ca54e3f61fded3a047d2d
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-10-21 18:10:03 +0900

    close #11

---
+++ server/app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'body',
+        'post_id',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo('App\Models\Post');
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
---
+++ server/app/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected $fillable = [
+        'email',
+        'password',
+        'display_name',
+        'gender_id',
+        'created_at',
+        'updated_at'
+    ];
+
+
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
+}
