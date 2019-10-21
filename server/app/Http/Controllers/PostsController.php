425892af98828efa3dbd14dc3e732f46a8fbab10
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-10-21 19:00:09 +0900

    create Service and get data list from Service

--- server/app/Http/Controllers/PostsController.php
+++ server/app/Http/Controllers/PostsController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Services\PostService;
 
 class PostsController extends Controller
 {
-    public function index()
-    {
-        $posts = Post::orderBy('created_at', 'desc')->get();
 
+    protected $service;
+
+    public function __construct(PostService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(Request $request)
+    {
+        $posts = $this->service->getPost();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
         return view('posts.index', ['posts' => $posts]);
     }
 }
