3bd738f4ecc74b11224cc0afc26926f03a8abf19
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-10-22 16:51:14 +0900

    method chnage resourse to match

--- server/routes/web.php
+++ server/routes/web.php
@@ -12,5 +12,4 @@
 */
 
 Route::match('get', '/', 'PostsController@index')->name('top');
-// Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
 Route::match('get', 'posts/create', 'PostsController@create')->name('create');
