afba8329195582b81804c13741613274f7eca11c
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-10-23 15:47:15 +0900

    create comments migration file

--- server/database/migrations/2019_10_23_064245_create_comments_table.php
+++ server/database/migrations/2019_10_23_064245_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id')->comment('ID');
             $table->unsignedInteger('user_id')->comment('投稿者のID');
-            $table->string('title', 50)->comment('タイトル');
-            $table->text('body')->comment('中身の文章');
+            $table->unsignedInteger('post_id')->comment('投稿のID');
+            $table->text('body')->comment('コメントの中身');
             $table->timestamps();
         });
-        DB::statement("ALTER TABLE `posts` COMMENT '投稿'");
+        DB::statement("ALTER TABLE `comments` COMMENT 'コメント'");
     }
 
     /**
@@ -30,6 +30,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 }
