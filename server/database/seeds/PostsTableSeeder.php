a8dc6b5b4da6851513df88fbc8e389cae2ce7876
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-10-23 16:29:25 +0900

    create seeder data posts and comments

---
+++ server/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            [
+                'title' => "post1",
+                'body' => "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext\ncontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontents",
+                'user_id' => 1,
+                'created_at' => date("Y-m-d H:i:s"),
+            ],
+            [
+                'title' => "post2",
+                'body' => "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext\texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextn",
+                'user_id' => 2,
+                'created_at' => date("Y-m-d H:i:s"),
+            ],
+            [
+                'title' => "post3",
+                'body' => "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext\texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttextn",
+                'user_id' => 2,
+                'created_at' => date("Y-m-d H:i:s"),
+            ]
+        ]);
+    }
+}
