f6ea2d335dc7f317ca9adf5c7ae5b1d4b4095e4a
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-10-24 19:33:18 +0900

    create log output

--- server/app/Services/UserService.php
+++ server/app/Services/UserService.php
@@ -11,6 +11,7 @@ class UserService
 
     public function getUserName($user_id)
     {
+        \Log::debug(__CLASS__ . '::' . __FUNCTION__ . ' called:(' . __LINE__ . ')');
         $user = ModelUser::findOrFail($user_id);
         $name = $user->name;
         return $name;
